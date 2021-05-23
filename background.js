a28b577c747e267e85d5856bbf41f3b1e63dc219
jkdihenkar <jkdihenkar@gmail.com>
2021-05-23 15:52:41 +0530

    attempt to make direct api call to notion

--- background.js
+++ background.js
@@ -24,6 +24,55 @@ function send_to_notion(info, tab) {
     var token = items.notion_token;
     var text = info.selectionText;
     var contextTabUrl = tab['url'];
-    console.log("Post data to Notion API", text, contextTabUrl);
-  });
+
+
+      // TODO: CORS issue when making authenticated api call
+        var t = fetch('https://api.notion.com/v1/blocks/c28a9b7af9904e3089587ab665999556/children', {
+            method: 'GET',
+            //mode: "no-cors",
+            credentials: "include",
+            headers: {
+                'Content-Type': 'application/json',
+                'Notion-Version': '2021-05-13',
+                'Authorization': "Bearer " + token,
+                'Access-Control-Allow-Origin': 'https://api.notion.com',
+                'Access-Control-Allow-Headers': 'Content-Type',
+                'Access-Control-Allow-Methods': 'GET, PUT, POST, OPTIONS'
+            }
+        })
+            .then(response =>  response.text())
+            .then(data => console.log(data));
+
+        writeData = {
+                "children": [
+                    {
+                        "object": "block",
+                        "type": "paragraph",
+                        "paragraph": {
+                            "text": [
+                                {
+                                    "type": "text",
+                                    "text": {
+                                        "content": text,
+                                        "link": { "url": contextTabUrl }
+                                    }
+                                }
+                            ]
+                        }
+                    }
+                ]
+            }
+        // fetch('https://api.notion.com/v1/blocks/c28a9b7af9904e3089587ab665999556/children', {
+        //     method: 'PATCH',
+        //     mode: "no-cors",
+        //     headers: {
+        //         'Content-Type': 'application/json',
+        //         'Notion-Version': '2021-05-13',
+        //         'Authorization': "Bearer " + token
+        //     },
+        //     body: JSON.stringify(writeData)
+        // })
+        //     .then(response => response.json())
+        //     .then(data => console.log(data));
+    });
 }
